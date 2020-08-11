fe8c3cce9b02df735e93375ed2a6be357c2beabb
tmarzec <tomasz4.marzec@student.uj.edu.pl>
2020-08-11 18:51:32 +0200

    better rpn parser

--- Parser.hs
+++ Parser.hs
@@ -14,14 +14,20 @@ fromRpn form = head . foldl folder [Variable $ head form] $ tail $ words form
             folder arr [x] = Variable x : arr
             folder _ _ = error "incorrect input ;c"
 
--- ["(", "p", "or", "q", ")"] -> "p q or"
+
+
+-- up to first "("
+dump :: ([String], [String]) -> ([String], [String])
+dump (a, "(" : xs) = (a, xs)
+dump (a, x : xs) = dump (x : a, xs)
+
 toRpn :: [String] -> String
-toRpn strs = unwords $ reverse (if null a then fs else (let [x] = a in x) : fs)
-    where   func a "(" = a
-            func (acc, x : xs) ")" = (x : acc, xs)
-            func (acc, ops) [v] = ([v] : acc, ops)
-            func (acc, ops) sth = (acc, sth : ops)
-            (fs, a) = foldl func ([], []) strs
+toRpn str = (unwords . reverse . fst) (foldl func ([], []) (("(" : str) ++ [")"]))
+    where   func (a, b) "(" = (a, "(" : b)
+            func (a, b) ")" = dump (a, b)
+            func (a, b) [v] = ([v] : a, b)
+            func (a, b) op = (a, op : b)
+
 
 -- "(p) or (q)" -> ["(","p",")","or","(","q",")"]
 clean :: String -> [String]
@@ -50,4 +56,15 @@ getLiteral x = error $ "wrong literal! " ++ x
 parseCnf :: String -> Cnf
 parseCnf str = map (Clause . map getLiteral . splitOn ",") (parseOut str)
 
+-}
+
+{-
+-- ["(", "p", "or", "q", ")"] -> "p q or"
+toRpn :: [String] -> String
+toRpn strs = unwords $ reverse (if null a then fs else (let [x] = a in x) : fs)
+    where   func a "(" = a
+            func (acc, x : xs) ")" = (x : acc, xs)
+            func (acc, ops) [v] = ([v] : acc, ops)
+            func (acc, ops) sth = (acc, sth : ops)
+            (fs, a) = foldl func ([], []) strs
 -}
